a5408c0027a4620efd2615e7acb4de91592cbe16
ateixieraleal <andersonpains@yahoo.com.br>
2021-06-26 16:26:20 -0300

    Rotas criadas

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require("express")
 const mongoose = require("mongoose")
 
+const tarefa_controller = require('./tarefas-controller')
+
+// Efetua a conexão com o Banco de Dados criado no MongoDB Atlas.
+mongoose.connect('mongodb+srv://ateixeiraleal:RkzVdrIPpkTRHl8c@cluster0.wwvsb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.Promise = global.Promise
+try {
+    let db = mongoose.connection
+    db.on('errr', console.error.bind(console, 'erro de conexao no banco'))
+} catch (e) {
+    console.log(e)
+}
+
+const router = express.Router()
+
 const app = express()
 
 app.use(express.urlencoded({ extended: true }));
@@ -9,6 +23,14 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('teste oi')
 })
+
+router.post('/tarefas', tarefa_controller.cadastrarTarefa)
+router.get('/tarefas', tarefa_controller.listarTarefas)
+router.get('/tarefas/:id', tarefa_controller.buscarTarefa)
+router.put('/tarefas/:id', tarefa_controller.alterarTarefa)
+router.delete('/tarefas/:id', tarefa_controller.deletarTarefa)
+
+app.use('/', router)
 
 /* Se houver uma variável de ambiente, denominada porta, no sistema onde a aplicação estiver rodando,
 ela será utilizada. E caso ela seja nula, a porta 3000 será utilizada. */
